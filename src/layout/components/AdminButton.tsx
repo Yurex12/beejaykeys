740cc160f55953b39e83580115cc83f26a5b13eb
Yurex <yusufekungomi@gmail.com>
2026-02-16 12:47:25 +0100

    fix header

--- src/layout/components/AdminButton.tsx
+++ src/layout/components/AdminButton.tsx
@@ -1,30 +1,21 @@
-import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useUser } from "@/features/auth/hooks/useUser";
 import { HiDatabase } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 function AdminButton() {
-  const { isLoading, isAuthenticated } = useUser();
+  const { isLoading } = useUser();
 
-  if (isLoading) return <Skeleton className="h-10 w-20" />;
+  if (isLoading) return <Skeleton className="h-9 w-40" />;
 
   return (
-    <div>
-      {isAuthenticated ? (
-        <Link
-          to="/dashboard/overview"
-          className="flex items-center justify-center gap-x-2 rounded-md border border-gray-600 px-4 py-2 text-sm text-gray-600"
-        >
-          <HiDatabase />
-          <span>Admin Dashboard</span>
-        </Link>
-      ) : (
-        <Button variant="outline" asChild>
-          <Link to="/login">Login</Link>
-        </Button>
-      )}
-    </div>
+    <Link
+      to="/dashboard/overview"
+      className="flex items-center justify-center gap-x-2 rounded-md border border-gray-600 px-4 py-2 text-sm text-gray-600"
+    >
+      <HiDatabase />
+      <span>Admin Dashboard</span>
+    </Link>
   );
 }
 
